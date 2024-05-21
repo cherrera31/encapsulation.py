7cb13f1f04b8c4a2d19041d2a04f41d28efd48d0
cherrera31 <carolina.herreraa20@gmail.com>
2024-05-20 21:07:25 -0700

    Add files via upload

---
+++ Mainencapsulacion.py
@@ -0,0 +1,8 @@
+from encapsulacion import Label
+
+label = Label("Frutas", "Arial")
+label.get_text()
+print(label.get_text())
+label.set_text("Verduras") #asignar un valor
+print (label.get_text())
+
